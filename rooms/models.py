196bb25754f3ec74fa2debf5edcfb85063da10e6
SpaceparticleJB <SpaceparticleJB@users.noreply.github.com>
2020-03-24 14:58:39 +0900

    ManyToManyField, RoomType

--- rooms/models.py
+++ rooms/models.py
@@ -3,11 +3,26 @@ from django_countries.fields import CountryField
 from core import models as core_models
 from users import models as user_models
 
-class Abstractitem(core_models)
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
 
 
-# Create your models here.
-class Room(models.Model):
+class RoomType(AbstractItem):
+
+    pass
+
+
+class Room(core_models.TimeStampedModel):
 
     """ Room Model Definition """
 
@@ -25,6 +40,7 @@ class Room(models.Model):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ManyToManyField(RoomType, blank=True)
 
     def __str__(self):
         return self.name
