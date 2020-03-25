9385d22f0cf878a44c9563f66733f338d9822f43
SpaceparticleJB <SpaceparticleJB@users.noreply.github.com>
2020-03-25 12:16:56 +0900

    on_delete, Amenity, Facility, HouseRule Models

--- rooms/admin.py
+++ rooms/admin.py
@@ -4,7 +4,7 @@ from . import models
 # Register your models here.
 
 
-@admin.register(models.RoomType)
+@admin.register(models.RoomType, models.Amenity, models.Facility, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
 
     pass
